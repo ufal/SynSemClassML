20cf9bb2583b5aac1d7e742403f8dff97db25ce8
Jana Strakova <strakova@ufal.mff.cuni.cz>
2025-01-31 14:10:29 +0100

    Increase default value for --max_frequency.

--- rank_unprocessed_lemmas_in_corpus.py
+++ rank_unprocessed_lemmas_in_corpus.py
@@ -78,7 +78,7 @@ if __name__ == "__main__":
     parser.add_argument("--langs_priority", default="ces,eng,spa,deu", type=str, help="Preferred order of allowed languages in SynSemClass class names displayed.")
     parser.add_argument("--load_model", default=None, type=str, help="Load model from directory.")
     parser.add_argument("--max_lines", default=10, type=int, help="Maximum corpus lines to process.")
-    parser.add_argument("--max_frequency", default=1000, type=int, help="Maximum number of lemma mentions to consider.")
+    parser.add_argument("--max_frequency", default=5000, type=int, help="Maximum number of lemma mentions to consider.")
     parser.add_argument("--output", default="lemma_suggestions.txt", type=str, help="Output file template.")
     parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
     parser.add_argument("--k", default=1, type=int, help="Top k classes.")
